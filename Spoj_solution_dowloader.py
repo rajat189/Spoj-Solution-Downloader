e21e0d9d8f14a6c0804784f9e212cb872e56eef3
Rajat Sharma <rajatsharma18994@gmail.com>
2017-09-22 18:22:09 +0530

    Spoj solutions
    Spoj solutions

--- Spoj_solution_dowloader.py
+++ Spoj_solution_dowloader.py
@@ -1,11 +1,12 @@
 import os
-import getpass
 import requests
 import grequests
 from bs4 import BeautifulSoup
 
 session = requests.session()
 
+USERNAME = ''
+PASSWORD = ''
 BASE_URL = "http://www.spoj.com"
 accepted_code = {}
 
@@ -13,14 +14,11 @@ accepted_code = {}
 # find/create valid path
 def basePath():
     while 1:
-        path = input('Enter path to save files: ').strip()
-        # complete home path
+        path = input('Enter a path to save all files: ').strip()
         if (path.startswith('~/')):
             path = path.replace('~', os.path.expanduser('~'), 1)
-
-        # create path if not exists
         if not os.path.exists(path):
-            print('Path not exists: ' + path)
+            print('Path does not exists: ' + path)
             permission = input('Do you want to create this path? (Y/N) ')
             if (permission.upper() == 'Y'):
                 os.makedirs(path)
@@ -38,35 +36,31 @@ def basePath():
 
 # save source code files
 def createFiles(results, problemCode):
-    path = basePath()  # path to save files
-    print('Writing files...')
+    path = basePath()
+    print('Saving files...')
     total = len(problemCode)
-    #print(results)
     no=0
     for i in range(total):
-        #print(results[i])
-        if results[i]==None:#print(problemCode[i]);
-            no+=1;continue
+        if results[i]==None:
+            no+=1
+            continue
         extension = results[i].headers['Content-Disposition'].split('-src')[1]
-        #print(extension,problemCode[i])
         sourceFile = open(path + problemCode[i] + extension, "w")
         sourceFile.write((results[i].text).encode('ascii', 'ignore').decode('ascii'))
         sourceFile.close()
-    print('Total files saved: ' + str(total))
-    print(no)
+    print('Total files saved: ' + str(no))
 
 # fetch all submissions
 def process(soup):
     problemCode = []
     problemUrl = []
-    r = session.get('http://www.spoj.com/status/rajat189/signedlist/')
+    r = session.get('http://www.spoj.com/status/'+USERNAME+'/signedlist/')
     signList = r.text
-    # print(signList)
-    f = open('a.txt', 'w+')
+    f = open('data.txt', 'w+')
     f.write(signList)
     f.close()
 
-    f = open('a.txt', 'r')
+    f = open('data.txt', 'r')
     data = f.readlines()
     for i in range(9, len(data) - 15):
         row = data[i].split('|')
@@ -76,22 +70,17 @@ def process(soup):
         if status == 'AC' or status.isnumeric():
             accepted_code[pid] = pcode
 
-    #print(accepted_code)
     for code in accepted_code:
         problemCode.append(accepted_code[code]+code)
         problemUrl.append('/files/src/save/'+code+'/')
-    #print(problemCode)
 
-    unsent_request = (grequests.get(BASE_URL + url, session=session) for url in problemUrl)
-    print(unsent_request)
-    results = grequests.map(unsent_request)
-
+    u_request = (grequests.get(BASE_URL + url, session=session) for url in problemUrl)
+    results = grequests.map(u_request)
+    os.remove('data.txt')
     createFiles(results, problemCode)
 
 
 def main():
-    USERNAME = ''
-    PASSWORD = ''
     payload={"login_user": USERNAME,"password": PASSWORD}
 
     print('Logging in...')
